d77fd78e81ca9378abbb91304aba5d782fe5abb9
watanabe <s.watanabe@sdk-co.com>
2021-05-07 18:45:20 +0900

    SSRを無効にするラッパーを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+/**
+ * SSRを無効にするためのラッパー
+ */
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <SafeHydrate><Component {...pageProps} /></SafeHydrate>
 }
 
 export default MyApp
