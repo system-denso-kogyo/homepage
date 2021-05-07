b1d8736b714da8dd21b225690468e35b371a2382
watanabe <s.watanabe@sdk-co.com>
2021-05-07 18:44:21 +0900

    headerにロゴを追加

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,21 @@
+import { Image } from '../components';
+import { css } from '@emotion/react'
+
+const logoCss = css`
+    width: 80px;
+    padding: 4px 4px 4px 10px;
+    position: absolute;
+`
+const Logo = () => <Image css={logoCss} src="logo.png"></Image>
+
+const headerCss = css`
+    height: 80px;
+`
+
 export const Header = () => {
     return (
-        <div></div>
+        <div css={headerCss}>
+            <Logo></Logo>
+        </div>
     );
 };
