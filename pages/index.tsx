eade8fc92852ef988ea39cfc1a1392cf9ecc98e5
watanabe <s.watanabe@sdk-co.com>
2021-05-07 18:45:39 +0900

    トップページに動画用のエリアを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,51 @@
 import Layout from '../components/layout/layout';
+import { Image } from '../components/image';
 import React from "react";
+import { css } from '@emotion/react';
+
+const videoCss = css`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+
+const Video = () => {
+    return <Image css={videoCss} src="top/thumbnail.png"></Image>
+}
+
+const videoAreaCss = css`
+    padding: 0;
+    max-height: 1080px;
+    min-height: 500px;
+`
+
+const VideoArea = () => {
+    // 表示域の高さを取得
+    const [outerHeight, setOuterHeight] = React.useState(window.outerHeight);
+    // windowのリサイズのたびに高さを更新
+    const updateOuterHeight = () => {
+        setOuterHeight(window.outerHeight);
+        console.log(window.outerHeight);
+    };
+    React.useEffect(() => {
+        window.addEventListener('resize', updateOuterHeight);
+        return () => window.removeEventListener('resize', updateOuterHeight);
+    });
+    // 表示域の高さをCSSで設定
+    const height = css`height: ${outerHeight - 140}px;`;
+
+    return <div css={[videoAreaCss, height]}>
+        <Video></Video>
+    </div>
+}
 
 export const Home = () => {
-  return <>
-    <Layout>
-    </Layout>
-  </>
+
+    return <>
+        <Layout>
+            <VideoArea></VideoArea>
+        </Layout>
+    </>
 }
 
 export default Home;
