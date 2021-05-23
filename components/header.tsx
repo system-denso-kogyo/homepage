ceb8d758958f001e94a846076d3542c062dd3e23
watanabe <s.watanabe@sdk-co.com>
2021-05-23 20:07:14 +0900

    headerを追従するよう変更

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import { Image, Menus } from '../components';
-import { css } from '@emotion/react'
+import { css } from '@emotion/react';
 import { Container, Row, Col } from '@mverissimoo/emotion-grid';
-import Link from 'next/link'
+import Link from 'next/link';
 import { MenuProps } from './elements/menus';
 
 const logoCss = css`
@@ -14,6 +14,10 @@ const Logo = () => <Image css={logoCss} src="logo.png"></Image>
 const headerCss = css`
     height: 80px;
     padding-top: 4px;
+    position: fixed;
+    z-index: 1;
+    background-color: #f1faee;
+    border-bottom: 1px solid #457b9d;
 `
 
 const menuProps: MenuProps = {
@@ -31,16 +35,16 @@ export const Header = () => {
     return (
         <Container css={headerCss} fluid>
             <Row>
-                <Col sm={1} css={{ padding: 0}} >
+                <Col sm={1} css={{ padding: 0 }} >
                     <Link href='/'><a><Logo></Logo></a></Link>
                 </Col>
-                <Col sm={3} css={{ lineHeight: 1.4}}>
+                <Col sm={3} css={{ lineHeight: 1.4 }}>
                     <Row><h2 css={{ margin: 0 }}>システム電装工業株式会社</h2></Row>
                     <Row><small>System Denso Kogyo Co.,ltd</small></Row>
                     <Row><small><a href='tel:0356036131'>TEL：03-5603-6131  ／ FAX：03-5603-6132</a></small></Row>
                 </Col>
                 <Col sm={4}>
-                    <Row noGutters css={{flexDirection: 'column'}}>
+                    <Row noGutters css={{ flexDirection: 'column' }}>
                         <Row>パンくずリスト</Row>
                         <Row justify={'center'} align={'center'} css={{ lineHeight: 1.4 }} noGutters>
                             <Menus {...menuProps}></Menus>
