ab6ca4ce48787df0950afd54959c8409d47d8d8f
watanabe <s.watanabe@sdk-co.com>
2021-05-28 19:52:38 +0900

    footerを作成

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,68 @@
-export const Footer = () => {
-    return (
-        <div></div>
-    );
-};
+import { Container, Row, Col } from '@mverissimoo/emotion-grid';
+import React from "react";
+import Link from 'next/link';
+
+interface LinksProps {
+    blocks: {
+        name: string
+        links: {
+            name: string
+            path: string
+        }[]
+    }[]
+}
+
+const LinkArea: React.FC<LinksProps> = ({ blocks }) =>
+    <Container css={{
+        height: '100px',
+        paddingTop: '10px',
+        paddingBottom: '10px'
+    }}>
+        <Row>
+            {blocks.map(({ name, links }) =>
+                <Col sm={2}>
+                    <Row>
+                        <Col css={{ lineHeight: '1.2rem' }}>
+                            <span css={{
+                                color: '#CDD6DD',
+                                fontSize: '0.8rem',
+                            }}>{name}</span>
+                        </Col>
+                        <Col sm={5}>
+                            {links.map(({ name, path }, id) =>
+                                <li key={id}
+                                    css={{
+                                        listStyleType: 'none',
+                                        fontSize: '0.8rem',
+                                        lineHeight: '1.2rem',
+                                    }}>
+                                    <Link href={path}><a css={{
+                                        color: 'white',
+                                    }}>{name}</a></Link>
+                                </li>
+                            )}
+                        </Col>
+                    </Row>
+                </Col>
+            )}
+        </Row>
+    </Container>
+
+const FooterBar = () =>
+    <div css={{ height: '30px' }}>
+        <span css={{
+            color: 'white',
+            fontSize: '0.6rem',
+            marginLeft: '20px'
+        }}>© 2020 System Denso Kogyo Co.,Ltd</span>
+    </div>
+
+interface FooterProps {
+    linksProps: LinksProps
+}
+
+export const Footer: React.FC<FooterProps> = ({ linksProps }) =>
+    <div css={{ backgroundColor: '#3B4043' }}>
+        <LinkArea {...linksProps}></LinkArea>
+        <FooterBar></FooterBar>
+    </div>
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -5,7 +5,42 @@ export const Layout: React.FC = ({ children }) => {
     return <>
         <Header></Header>
         <main css={{ marginBottom: '100px' }}>{children}</main>
-        <Footer></Footer>
+        <Footer {...{
+            linksProps: {
+                blocks: [
+                    {
+                        name: '企業情報',
+                        links: [
+                            { name: '企業情報', path: '' },
+                            { name: '企業理念', path: '' },
+                            { name: '社長あいさつ', path: '' },
+                            { name: '事業内容', path: '' },
+                            { name: 'アクセスマップ', path: '' },
+                        ]
+                    },
+                    {
+                        name: 'お知らせ',
+                        links: [
+                            { name: 'ニュースリリーストップ', path: '' }
+                        ]
+                    },
+                    {
+                        name: '採用情報',
+                        links: [
+                            { name: 'システム事業採用情報', path: '' },
+                            { name: '工事事業採用情報', path: '' }
+                        ]
+                    },
+                    {
+                        name: 'お問い合わせ',
+                        links: [
+                            { name: '03-5603-6131', path: '' },
+                            { name: 'jinji@sdk-co.com', path: '' }
+                        ]
+                    }
+                ]
+            }
+        }}></Footer>
     </>
 }
 
