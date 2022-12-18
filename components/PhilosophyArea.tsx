e813c2563703aef264120c234f8d1427cfff8f43
Kohei Sakurai <kohe.sakurai@gmail.com>
2022-12-18 18:56:43 +0900

    企業理念 更新: heading削除, style調整

--- components/PhilosophyArea.tsx
+++ components/PhilosophyArea.tsx
@@ -3,22 +3,17 @@ import { Container, Row, Col } from '@mverissimoo/emotion-grid';
 
 
 export interface PhilosophyProps {
-    philosophy: { heading: string, description: string }[]
+    philosophy: { description: string }[]
 }
 
 export const PhilosophyArea: React.FC<PhilosophyProps> = ({ philosophy }) =>
     <Container fluid css={{ color: '#1D3557', marginTop: '40px', marginBottom: '320px' }}>
-        {philosophy.map(({ heading, description }) =>
-            <Row justify={"center"} css={{ marginTop: '16px', marginBottom: '60px', fontFamily: '源ノ角ゴシック JP' }}>
+        {philosophy.map(({ description }) =>
+            <Row justify={"center"} css={{ marginTop: '16px', marginBottom: '32px' }}>
                 <Col sm={5.5}>
-                    <Row justify={"flex-start"} css={{ marginTop: '16px', fontSize: "2.1rem" }}>
+                    <Row justify={"flex-start"} css={{ marginTop: '0px', fontSize: "1.3rem" }}>
                         <Col>
-                            {heading}
-                        </Col>
-                    </Row>
-                    <Row justify={"flex-start"} css={{ marginTop: '0px', fontSize: "1.4rem" }}>
-                        <Col>
-                            {description}
+                            • {description}
                         </Col>
                     </Row>
                 </Col>
