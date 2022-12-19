aef7747acee996d165a953c7369ab60f6cde8590
Kohei Sakurai <kohe.sakurai@gmail.com>
2022-12-19 20:37:32 +0900

    事業内容 更新 (トップページ共通化)
    * components/elements
        * description.tsx: subtitle?追加, Description更新, DescriptionParagraph追加して呼ぶ:pをrender
        * achievements.tsx: AchievementsItemProps定義. left,right -> achievements
    * components
        * systemArea.tsx, equipmentArea.tsx 削除
        * businessDetailedUnitArea.tsx: BusinessDetailedUnitArea. 設備工事,システム開発 共通
        * introductionArea.tsx: system,construct削除, introduction:stringのみ
    * pages
        * index.tsx: 事業内容を SystemUnit, EquipmentUnitとして定数化し, business.tsxで参照
        * business.tsx: index.tsxの定義参照

--- components/elements/description.tsx
+++ components/elements/description.tsx
@@ -3,18 +3,26 @@ import Link from 'next/link';
 
 export interface DescriptionProps {
     title?: string
+    subtitle?: string
     description?: string
     path?: string
 }
 
-export const Description: React.FC<DescriptionProps> = ({ title, description, path }) =>
+export const DescriptionParagraph: React.FC<{description:string}> = ({description}) => 
+    <p css={{
+        fontSize: '1.3rem',
+        lineHeight: '1.2',
+        marginTop: '8px',
+        marginBottom: '8px',
+    }}>{description}</p>
+
+export const Description: React.FC<DescriptionProps> = ({ title, subtitle, description, path }) => 
     <>
-        <h2 css={{ textAlign: "center", margin: 0 }}>{title}</h2>
-        <p css={{
-            fontSize: '1.3rem',
-            lineHeight: '1.2',
-            marginTop: '8px',
-            marginBottom: '8px',
-        }}>{description}</p>
-        {path !== undefined && <Link href={path}><a css={{ fontWeight: "bold", cursor: 'pointer' }}>詳しく見る</a></Link>}
+        <h2 css={{ textAlign: "left", margin: 0 }}>{title}<br></br>
+            {subtitle !== undefined && <span css={{
+                'margin-left':'10px', fontSize:'1.1rem'}}>{subtitle}</span>}
+        </h2> 
+        <DescriptionParagraph description={description}></DescriptionParagraph>
+        {path !== undefined && 
+            <Link href={path}><a css={{ fontWeight: "bold", cursor: 'pointer' }}>詳しく見る</a></Link>}
     </>
